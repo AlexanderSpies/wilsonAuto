923b997399c23c6dab2ddb284aed52d5a1a07361
Alex Spies <97462738+AlexanderSpies@users.noreply.github.com>
2024-06-17 22:19:53 -0400

    delete stuff

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import LandingPage from './landingPage/landingPage.jsx';
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
 import {ChakraProvider} from '@chakra-ui/react';
+
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
